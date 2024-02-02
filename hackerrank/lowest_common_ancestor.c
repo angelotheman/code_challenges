00643d0c5f1e3a2d7b8869413b84c043d4de045d
Kwabena Twumasi <ktwum721@gmail.com>
2024-02-02 17:08:20 +0000

    Lowest common ancestor

--- hackerrank/lowest_common_ancestor.c
+++ hackerrank/lowest_common_ancestor.c
@@ -14,21 +14,13 @@ struct node *lca( struct node *root, int v1, int v2 ) {
     if (!root)
         return (NULL);
         
-    if (root->data == v1 || root->data == v2)
+    if (v1 < root->data && v2 < root->data)
     {
-        return (root);
-    }
-    else if ((v1 < root->data && v2 > root->data) || (v1 > root->data && v2 < root->data))
-    {
-        return (root);
-    }
-    else if (v1 < root->data && v2 < root->data)
-    {
-        lca(root->left, v1, v2);
+        return (lca(root->left, v1, v2));
     }
     else if (v1 > root->data && v2 > root->data)
     {
-        lca(root->right, v1, v2);
+        return (lca(root->right, v1, v2));
     }
     
     return (root);
