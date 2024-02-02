1f03436afb9c1793f49d7d57e7eff491fa4b2eed
Kwabena Twumasi <ktwum721@gmail.com>
2024-02-02 12:41:08 +0000

    Leet code 700

---
+++ leetcode/700.c
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+struct TreeNode* searchBST(struct TreeNode* root, int val) {
+    if (!root)
+        return (NULL);
+    
+    if (val < root->val)
+        return searchBST(root->left, val);
+    else if (val > root->val)
+        return searchBST(root->right, val);
+    else
+        return (root);
+}
